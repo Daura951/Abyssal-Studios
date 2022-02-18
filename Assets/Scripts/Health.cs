47d60d2fc6261839dd3f0a1f5d4ea4f993cb2a21
Sooknav <sooknav@sunypoly.edu>
2022-02-18 12:02:45 -0500

    2/18 Class
    Flying and rough respawn

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
@@ -29,7 +30,14 @@ public class Health : MonoBehaviour
             }
             else
             {
-                Destroy(gameObject);
+                if (gameObject.tag == "Player")
+                {
+                    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+                }
+                else
+                {
+                    Destroy(gameObject);
+                }
             }
             
 
--- Assets/Scripts/Patrols.cs
+++ Assets/Scripts/Patrols.cs
@@ -6,18 +6,25 @@ public class Patrols : MonoBehaviour
 {
     [SerializeField] private Transform leftBound;
     [SerializeField] private Transform rightBound;
+    [SerializeField] private Transform upperBound;
+    [SerializeField] private Transform lowerBound;
 
     [SerializeField] private Transform enemy;
     [SerializeField] private float speed;
+    [SerializeField] private float flySpeed;
+    [SerializeField] private bool flying;
 
     private bool leftFace;
+    private bool risingEdge;
 
-
+    private int flap;
     private Vector3 initialDirection;
 
     private void Awake()
     {
         initialDirection = enemy.localScale;
+        flap = 1;
+        risingEdge = true;
     }
     
     private void Update()
@@ -27,7 +34,7 @@ public class Patrols : MonoBehaviour
 
             if (enemy.position.x >= leftBound.position.x)
             {
-                MoveInDirection(-1);
+                MoveInDirection(-1, flap);
             }
             else
             {
@@ -39,7 +46,7 @@ public class Patrols : MonoBehaviour
         {
             if (enemy.position.x <= rightBound.position.x)
             {
-                MoveInDirection(1);
+                MoveInDirection(1, flap);
             }
             else
             {
@@ -48,13 +55,34 @@ public class Patrols : MonoBehaviour
 
         }
 
+        if (flying)
+        {
+            if (risingEdge)
+            {
+                if (enemy.position.y >= upperBound.position.y)
+                {
+                    flap = -1;
+                    risingEdge = false;
+                }
+            }
+
+            else
+            {
+                if (enemy.position.y <= lowerBound.position.y)
+                {
+                    flap = 1;
+                    risingEdge = true;
+                }
+            }
+        }
+
     } 
 
-    private void MoveInDirection(int _direction)
+    private void MoveInDirection(int _direction, int _fly)
     {
        enemy.localScale = new Vector3(Mathf.Abs(initialDirection.x) * _direction, initialDirection.y, initialDirection.z);
         
-       enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y, enemy.position.z);
+       enemy.position = new Vector3(enemy.position.x + Time.deltaTime * _direction * speed, enemy.position.y +Time.deltaTime * _fly * flySpeed, enemy.position.z);
         
     }
 
